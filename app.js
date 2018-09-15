ab970d536dd3f4b7f49f1e7653ee4d618cd7b4c8
arnesh-kumar <arnesh.kumar@ca.com>
2018-09-15 14:35:04 +0530

    adding greetings

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const moment = require('moment');
 
 const app = express();
 
@@ -9,11 +10,18 @@ app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
     let requestName = req.query.name;
-
+    let time = moment().utcOffset(330).format('hh:mm:ss');
+    let greeting = '';
+    if (time < '06:00:00'){
+        greeting = 'afternoon';
+    }else if(time < '12:00:00') {
+        greeting = "morning";
+    }
+    console.log(time);
     if (!requestName){
-        res.send('Hello, user');
+        res.send('Hello, user, Good ' + greeting );
     }else {
-        res.send('HELLO, ' + requestName);
+        res.send('HELLO, ' + requestName + '  Good ' + greeting);
     }
 });
 
